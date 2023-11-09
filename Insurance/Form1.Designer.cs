fec9932fc2e07da4e305c09aa8c83ef84e92bb63
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2023-11-09 13:28:17 +0300

    Добавьте файлы проекта.

---
+++ Insurance/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Insurance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.but_add = new System.Windows.Forms.Button();
+            this.but_redact = new System.Windows.Forms.Button();
+            this.but_delete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.but_save = new System.Windows.Forms.Button();
+            this.but_cancel = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(630, 207);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // but_add
+            // 
+            this.but_add.Location = new System.Drawing.Point(22, 547);
+            this.but_add.Name = "but_add";
+            this.but_add.Size = new System.Drawing.Size(103, 30);
+            this.but_add.TabIndex = 1;
+            this.but_add.Text = "Добавить";
+            this.but_add.UseVisualStyleBackColor = true;
+            this.but_add.Click += new System.EventHandler(this.but_add_click);
+            // 
+            // but_redact
+            // 
+            this.but_redact.Enabled = false;
+            this.but_redact.Location = new System.Drawing.Point(157, 547);
+            this.but_redact.Name = "but_redact";
+            this.but_redact.Size = new System.Drawing.Size(125, 30);
+            this.but_redact.TabIndex = 2;
+            this.but_redact.Text = "Редактировать";
+            this.but_redact.UseVisualStyleBackColor = true;
+            this.but_redact.Click += new System.EventHandler(this.but_redact_Click);
+            // 
+            // but_delete
+            // 
+            this.but_delete.Enabled = false;
+            this.but_delete.Location = new System.Drawing.Point(304, 547);
+            this.but_delete.Name = "but_delete";
+            this.but_delete.Size = new System.Drawing.Size(103, 30);
+            this.but_delete.TabIndex = 3;
+            this.but_delete.Text = "Удалить";
+            this.but_delete.UseVisualStyleBackColor = true;
+            this.but_delete.Click += new System.EventHandler(this.but_delete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ФИО";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 257);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(282, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Серия номер паспорта";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(370, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(269, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(384, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Телефон";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(22, 311);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(282, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Дата рождения";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(370, 311);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(269, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 413);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Вид страхования";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(22, 372);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(282, 22);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(387, 413);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(201, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Номер страхового документа";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(370, 372);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(269, 22);
+            this.textBox6.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 476);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(211, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Дата оформления страхования";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(22, 435);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(282, 22);
+            this.textBox7.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(384, 476);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(167, 16);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Дата конца страхования";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(370, 435);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(269, 22);
+            this.textBox8.TabIndex = 19;
+            // 
+            // but_save
+            // 
+            this.but_save.Enabled = false;
+            this.but_save.Location = new System.Drawing.Point(437, 547);
+            this.but_save.Name = "but_save";
+            this.but_save.Size = new System.Drawing.Size(96, 30);
+            this.but_save.TabIndex = 20;
+            this.but_save.Text = "Сохранить";
+            this.but_save.UseVisualStyleBackColor = true;
+            this.but_save.Click += new System.EventHandler(this.but_save_Click);
+            // 
+            // but_cancel
+            // 
+            this.but_cancel.Enabled = false;
+            this.but_cancel.Location = new System.Drawing.Point(564, 547);
+            this.but_cancel.Name = "but_cancel";
+            this.but_cancel.Size = new System.Drawing.Size(75, 30);
+            this.but_cancel.TabIndex = 21;
+            this.but_cancel.Text = "Отмена";
+            this.but_cancel.UseVisualStyleBackColor = true;
+            this.but_cancel.Click += new System.EventHandler(this.but_cancel_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(22, 495);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(282, 22);
+            this.textBox9.TabIndex = 22;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(370, 495);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(269, 22);
+            this.textBox10.TabIndex = 23;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(56, 350);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 16);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Адрес проживания";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(384, 350);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(255, 16);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Номер водительского удостоверения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 606);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.but_cancel);
+            this.Controls.Add(this.but_save);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.but_delete);
+            this.Controls.Add(this.but_redact);
+            this.Controls.Add(this.but_add);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Страхование насиления";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button but_add;
+        private System.Windows.Forms.Button but_redact;
+        private System.Windows.Forms.Button but_delete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button but_save;
+        private System.Windows.Forms.Button but_cancel;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Insurance/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance
+{
+    public class Person
+    {
+        public string name;
+        public string nember;
+        public string passport;
+        public string age;
+        public string adres;
+        public string nember_drive;
+        public string insurance;
+        public string insurance_nember;
+        public string date_start;
+        public string date_end;
+        public string path;
+
+
+        public Person(string name, string nember, string passport, string age, string adres,string nember_drive, string insurance, string insurance_nember, string date_start, string date_end, string path)
+        {
+            this.name = name;
+            this.nember = nember;
+            this.passport = passport;
+            this.age = age;
+            this.adres = adres;
+            this.nember_drive = nember_drive;
+            this.insurance = insurance;
+            this.insurance_nember = insurance_nember;
+            this.date_start = date_start;
+            this.date_end = date_end;
+            this.path = path;
+        }
+        public Person()
+        {
+
+        }
+    }
+}
